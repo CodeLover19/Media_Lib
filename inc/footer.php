7d8a5154c8d092c8f8b61f299a16144bba77f2f8
CodeLover19 <CodeLover19@users.noreply.github.com>
2018-10-18 23:28:48 -0700

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/leepaul24">Twitter</a></li>
+				<li><a href="https://www.facebook.com/">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Media Collection</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
